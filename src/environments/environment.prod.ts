d4403945f549ea05dbffc64bb51a26130c758d00
Robert I <robertivascu24@gmail.com>
2023-07-07 00:44:24 +0300

    Wip

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCHcaPQNaOoHxnN-Thi8yQE5VLF1rCk8xc",
+    authDomain: "azet-dt.firebaseapp.com",
+    projectId: "azet-dt",
+    storageBucket: "azet-dt.appspot.com",
+    messagingSenderId: "188014979372",
+    appId: "1:188014979372:web:bca4339e9362673257a489",
+    measurementId: "G-NKDLFKH348",
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCHcaPQNaOoHxnN-Thi8yQE5VLF1rCk8xc",
+    authDomain: "azet-dt.firebaseapp.com",
+    projectId: "azet-dt",
+    storageBucket: "azet-dt.appspot.com",
+    messagingSenderId: "188014979372",
+    appId: "1:188014979372:web:bca4339e9362673257a489",
+    measurementId: "G-NKDLFKH348",
+  },
+};
